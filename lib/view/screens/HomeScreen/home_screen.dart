a1f31f91b123fdfa758d589eb51db5d58a3ecc93
eightgran <eightgran@gmail.com>
2021-08-02 19:20:16 +0200

    Implement persistent `tabIndex`

--- lib/view/screens/HomeScreen/home_screen.dart
+++ lib/view/screens/HomeScreen/home_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:history_of_me/config/config.dart';
+import 'package:history_of_me/controller/database/hive_db_service.dart';
+import 'package:history_of_me/model/app_settings.dart';
+import 'package:hive/hive.dart';
 import 'package:leitmotif/leitmotif.dart';
 
 import '../screens.dart';
 
-/// A navigatable screen [Widget].
+/// The app's home screen widget allowing to navigate between multiple tabs.
 ///
-/// Displays the currently user selected tab. The navigation inside the
-/// [HomeScreen] will be performed using a [LitBottomNavigation].
+/// Accessing and creating (if required) the [AppSettings] will be handled
+/// within the app.
 class HomeScreen extends StatefulWidget {
   /// The bookmark animation's [Duration].
   final Duration bookmarkAnimationDuration;
@@ -26,9 +30,22 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   late AnimationController _bookmarkAnimation;
 
+  late HiveDBService _hiveDBService;
+
+  /// Persists the `tabIndex` on the the corresponding [AppSettings] instance.
+  void _onTabSwitch(int tabIndex, AppSettings appSettings) {
+    _hiveDBService.updateTabIndex(appSettings, tabIndex);
+  }
+
+  /// Creates the [AppSettings] instance.
+  void _createAppSettings() {
+    _hiveDBService.createAppSettings();
+  }
+
   @override
   void initState() {
     super.initState();
+    _hiveDBService = HiveDBService();
     _bookmarkAnimation = AnimationController(
       duration: widget.bookmarkAnimationDuration,
       vsync: this,
@@ -44,29 +61,52 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return LitTabView(
-      tabs: [
-        LitNavigableTab(
-          tabData: LitBottomNavigationTabData(
-            index: 0,
-            icon: LitIcons.home,
-            iconSelected: LitIcons.home_alt,
-          ),
-          screen: DiaryScreen(
-            bookmarkAnimation: _bookmarkAnimation,
-          ),
-        ),
-        LitNavigableTab(
-          tabData: LitBottomNavigationTabData(
-            index: 1,
-            icon: LitIcons.person,
-            iconSelected: LitIcons.person_solid,
-          ),
-          screen: ProfileScreen(
-            bookmarkAnimation: _bookmarkAnimation,
-          ),
-        )
-      ],
+    return ValueListenableBuilder(
+      valueListenable: _hiveDBService.getAppSettings(),
+      builder: (context, Box<AppSettings> appSettingsBox, _) {
+        AppSettings appSettings = AppSettings(
+          privacyPolicyAgreed: initialAgreedPrivacy,
+          darkMode: initialDarkMode,
+          tabIndex: initialTabIndex,
+        );
+        // Try to retrieve the `AppSettings` instance
+        try {
+          appSettings = appSettingsBox.getAt(0)!;
+          // Ensure to create `AppSettings` instance if it's missing.
+        } catch (e) {
+          print(e);
+          _createAppSettings();
+          print('Error while accessing AppSettings object. '
+              'Creating backup AppSettings object ...');
+        }
+
+        return LitTabView(
+          initialTabIndex: appSettings.tabIndex,
+          transitionListener: (index) => _onTabSwitch(index, appSettings),
+          tabs: [
+            LitNavigableTab(
+              tabData: LitBottomNavigationTabData(
+                index: 0,
+                icon: LitIcons.home,
+                iconSelected: LitIcons.home_alt,
+              ),
+              screen: DiaryScreen(
+                bookmarkAnimation: _bookmarkAnimation,
+              ),
+            ),
+            LitNavigableTab(
+              tabData: LitBottomNavigationTabData(
+                index: 1,
+                icon: LitIcons.person,
+                iconSelected: LitIcons.person_solid,
+              ),
+              screen: ProfileScreen(
+                bookmarkAnimation: _bookmarkAnimation,
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
