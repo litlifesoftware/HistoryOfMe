06a3c846de9ab6d3cfc975f6771ef14f6f0d05dd
eightgran <eightgran@gmail.com>
2021-08-21 19:49:00 +0200

    Add documentation on `UserCreatedColor`

--- lib/model/user_created_color.dart
+++ lib/model/user_created_color.dart
@@ -2,6 +2,9 @@ import 'package:hive/hive.dart';
 
 part 'user_created_color.g.dart';
 
+/// A model class storing an user-created `Color` object.
+///
+/// Each color channel is extracted into its own property.
 @HiveType(typeId: 1)
 class UserCreatedColor {
   @HiveField(0)
@@ -15,6 +18,7 @@ class UserCreatedColor {
   @HiveField(4)
   final int blue;
 
+  /// Creates an [UserCreatedColor] object.
   const UserCreatedColor({
     required this.uid,
     required this.alpha,
@@ -23,6 +27,10 @@ class UserCreatedColor {
     required this.blue,
   });
 
+  /// Creates an [UserCreatedColor] object by serializing the provided `Map`
+  /// data.
+  ///
+  /// `JSON` decoding must be done before serialization.
   factory UserCreatedColor.fromJson(Map<String, dynamic> json) {
     return UserCreatedColor(
       uid: json['uid'] as String,
@@ -33,6 +41,9 @@ class UserCreatedColor {
     );
   }
 
+  /// Creates a `Map` object based on this [UserCreatedColor] object.
+  ///
+  /// `JSON` encoding must be done after serialization.
   Map<String, dynamic> toJson() => {
         'uid': uid,
         'alpha': alpha,
