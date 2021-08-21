085f428ee2e13b516f76c4ac2b66d86630e6d30e
eightgran <eightgran@gmail.com>
2021-08-21 19:46:14 +0200

    Add documentation on `AppSettings`

--- lib/model/app_settings.dart
+++ lib/model/app_settings.dart
@@ -2,6 +2,7 @@ import 'package:hive/hive.dart';
 
 part 'app_settings.g.dart';
 
+/// A model class storing app-specific data, which may only be client-specific.
 @HiveType(typeId: 3)
 class AppSettings {
   @HiveField(0)
@@ -10,12 +11,17 @@ class AppSettings {
   final bool darkMode;
   @HiveField(2)
   final int tabIndex;
+
+  /// Creates a [AppSettings] object.
   const AppSettings({
     required this.privacyPolicyAgreed,
     required this.darkMode,
     required this.tabIndex,
   });
 
+  /// Creates a [AppSettings] object by serializing the provided `Map` data.
+  ///
+  /// `JSON` decoding must be done before serialization.
   factory AppSettings.fromJson(Map<String, dynamic> json) {
     return AppSettings(
       privacyPolicyAgreed: json['privacyPolicyAgreed'] as bool,
@@ -24,6 +30,9 @@ class AppSettings {
     );
   }
 
+  /// Creates a `Map` object based on this [AppSettings] object.
+  ///
+  /// `JSON` encoding must be done after serialization.
   Map<String, dynamic> toJson() => {
         'privacyPolicyAgreed': privacyPolicyAgreed,
         'darkMode': darkMode,
