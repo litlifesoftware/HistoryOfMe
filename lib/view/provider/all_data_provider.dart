5ab35c5d1756fdae71b9f340062ef069d07e6415
eightgran <eightgran@gmail.com>
2021-08-26 00:33:31 +0200

    Add `AllDataProvider`

---
+++ lib/view/provider/all_data_provider.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:history_of_me/controller/controllers.dart';
+import 'package:history_of_me/model/models.dart';
+import 'package:hive/hive.dart';
+
+/// A `provider` widget allowing to pass all data stored on the `Hive` database
+/// onto its child Widget using the [builder] method.
+///
+/// Accessing all data at once is rather expensive on memory but required to
+/// e.g. create backupable objects.
+///
+class AllDataProvider extends StatefulWidget {
+  /// `builder` method allowing to access all data available on its child.
+  final Widget Function(
+      BuildContext context,
+      AppSettings appSettings,
+      UserData userData,
+      List<DiaryEntry> diaryEntries,
+      List<UserCreatedColor> userCreatedColors) builder;
+
+  /// Creates an [AllDataProvider] that provides all data stored on the `Hive`
+  /// database.
+  const AllDataProvider({
+    Key? key,
+    required this.builder,
+  }) : super(key: key);
+
+  @override
+  _AllDataProviderState createState() => _AllDataProviderState();
+}
+
+class _AllDataProviderState extends State<AllDataProvider> {
+  final HiveDBService _service = HiveDBService();
+
+  /// Extract the main object used from the [AppSettings]'s `Box`.
+
+  AppSettings _getAppSettings(Box<AppSettings> appSettingsBox) {
+    return appSettingsBox.getAt(0)!;
+  }
+
+  /// Extract the main object used from the [UserData]'s `Box`.
+
+  UserData _getUserData(Box<UserData> userDataBox) {
+    return userDataBox.getAt(0)!;
+  }
+
+  /// Extracts the [DiaryEntry]'s `Box` content into a `List`.
+
+  List<DiaryEntry> _getEntries(Box<DiaryEntry> diaryEntryBox) {
+    return diaryEntryBox.values.toList();
+  }
+
+  /// Extracts the [UserCreatedColor]'s `Box` content into a `List`.
+
+  List<UserCreatedColor> _getCreatedColors(
+    Box<UserCreatedColor> userCreatedColorBox,
+  ) {
+    return userCreatedColorBox.values.toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: _service.getAppSettings(),
+      builder: (context, Box<AppSettings> appSettingsBox, _) {
+        return ValueListenableBuilder(
+          valueListenable: _service.getUserData(),
+          builder: (context, Box<UserData> userDataBox, _) {
+            return ValueListenableBuilder(
+              valueListenable: _service.getDiaryEntries(),
+              builder: (context, Box<DiaryEntry> diaryEntryBox, _) {
+                return ValueListenableBuilder(
+                  valueListenable: _service.getUserCreatedColors(),
+                  builder:
+                      (context, Box<UserCreatedColor> userCreatedColorBox, _) {
+                    final UserData userData = _getUserData(userDataBox);
+                    final appSettings = _getAppSettings(appSettingsBox);
+                    final diaryEntries = _getEntries(diaryEntryBox);
+                    final userCreatedColors =
+                        _getCreatedColors(userCreatedColorBox);
+
+                    return widget.builder(
+                      context,
+                      appSettings,
+                      userData,
+                      diaryEntries,
+                      userCreatedColors,
+                    );
+                  },
+                );
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+}
