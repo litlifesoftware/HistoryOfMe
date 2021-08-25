8faba2815c2fe424e25613ccdc3f2e394c43d1f0
eightgran <eightgran@gmail.com>
2021-08-26 00:13:45 +0200

    Add `controllers` library

---
+++ lib/controller/controllers.dart
@@ -0,0 +1,10 @@
+/// A list of controller classes used to implement certain features using
+/// business logic.
+library controllers;
+
+export 'database/hive_db_service.dart';
+export 'database/hive_query_controller.dart';
+export 'localization/hom_localizations.dart';
+export 'routes/hom_navigator.dart';
+export 'routes/url_lauch_controller.dart';
+export 'util/color/inverted_color_brightness.dart';
