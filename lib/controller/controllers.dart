b66d5b99611a3c675e1a878198006697bb1f2f7c
eightgran <eightgran@gmail.com>
2021-09-18 19:26:47 +0200

    Remove  `InvertedColorBrightness`

--- lib/controller/controllers.dart
+++ lib/controller/controllers.dart
@@ -7,4 +7,3 @@ export 'database/hive_db_service.dart';
 export 'database/hive_query_controller.dart';
 export 'localization/hom_localizations.dart';
 export 'routes/hom_navigator.dart';
-export 'util/color/inverted_color_brightness.dart';
