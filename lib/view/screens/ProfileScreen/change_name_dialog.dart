ec946a4e36171497700cffbf8f3e027fb6302642
eightgran <eightgran@gmail.com>
2022-01-17 22:04:56 +0100

    Refactor `ChangeNameDialog` - Implement AppAPI library - Remove input value variable

--- lib/view/screens/ProfileScreen/change_name_dialog.dart
+++ lib/view/screens/ProfileScreen/change_name_dialog.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:history_of_me/controller/database/hive_db_service.dart';
+import 'package:history_of_me/api.dart';
 import 'package:history_of_me/localization.dart';
 import 'package:history_of_me/model/user_data.dart';
+import 'package:history_of_me/widgets.dart';
 import 'package:leitmotif/leitmotif.dart';
 
+/// A dialog widget allowing the user to change his name.
 class ChangeNameDialog extends StatefulWidget {
-  final UserData? userData;
+  /// The user's data.
+  final UserData userData;
 
+  /// Creates a [ChangeNameDialog].
   const ChangeNameDialog({
     Key? key,
     required this.userData,
@@ -17,43 +21,67 @@ class ChangeNameDialog extends StatefulWidget {
 }
 
 class _ChangeNameDialogState extends State<ChangeNameDialog> {
-  late TextEditingController _nameController;
-  late FocusNode _focusNode;
-  late String _updatedName;
+  late AppAPI _api;
 
+  /// Controlls the `name` input value.
+  late TextEditingController _nameController;
+
+  /// Controlls the text field focus.
+  late FocusNode _focusNode;
+
+  /// Returns whether the input is different from the initial value.
+  bool get _isChanged {
+    print(_nameController.text != widget.userData.name);
+    return _nameController.text != widget.userData.name;
+  }
+
+  /// Handles the `cancel` action.
   void _onCancel() {
     LitRouteController(context).closeDialog();
   }
 
-  bool get _isChanged {
-    return _nameController.text != widget.userData!.name;
-  }
-
+  /// Defocuses the text field.
   void _defocus() {
     LitFocusController(context).defocus();
-    _onChange();
   }
 
-  void _onChange() {
-    setState(() {
-      _updatedName = _nameController.text;
-    });
-  }
-
+  /// Submits the updated user value.
   void _onSubmit() {
-    HiveDBService().updateUsername(widget.userData!, _updatedName);
+    _api.updateUsername(widget.userData, _nameController.text);
     LitRouteController(context).closeDialog();
   }
+
+  /// Forces to rebuild the widget.
+  ///
+  /// The validation [_isChanged] requires the widget to rebuild after each
+  /// keystroke to ensure the `save` button gets enabled.
+  void _forceRebuild() {
+    setState(() => {});
+  }
+
+  ActionButtonData get _cancelButtonData => ActionButtonData(
+        title: LeitmotifLocalizations.of(context).cancelLabel,
+        onPressed: _onCancel,
+      );
+
+  ActionButtonData get _saveButtonData => ActionButtonData(
+        title: LeitmotifLocalizations.of(context).applyLabel,
+        accentColor: Color(0xFFEDDEC0),
+        backgroundColor: Color(0xFFEAEACA),
+        disabled: !_isChanged,
+        onPressed: _onSubmit,
+      );
 
   @override
   void initState() {
     super.initState();
 
-    _nameController = TextEditingController(
-      text: widget.userData!.name,
-    )..addListener(_onChange);
+    _api = AppAPI();
 
-    _updatedName = widget.userData!.name;
+    _nameController = TextEditingController(
+      text: widget.userData.name,
+    )..addListener(_forceRebuild);
+
     _focusNode = FocusNode();
   }
 
@@ -63,20 +91,9 @@ class _ChangeNameDialogState extends State<ChangeNameDialog> {
       titleText: AppLocalizations.of(context).changeYourNameLabel,
       actionButtons: [
         DialogActionButton(
-          data: ActionButtonData(
-            title: LeitmotifLocalizations.of(context).cancelLabel,
-            onPressed: _onCancel,
-          ),
+          data: _cancelButtonData,
         ),
-        DialogActionButton(
-          data: ActionButtonData(
-            title: LeitmotifLocalizations.of(context).applyLabel,
-            accentColor: Color(0xFFEDDEC0),
-            backgroundColor: Color(0xFFEAEACA),
-            disabled: !_isChanged,
-            onPressed: _onSubmit,
-          ),
-        ),
+        DialogActionButton(data: _saveButtonData),
       ],
       child: CleanInkWell(
         onTap: _defocus,
@@ -84,10 +101,7 @@ class _ChangeNameDialogState extends State<ChangeNameDialog> {
           child: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16.0,
-                  vertical: 16.0,
-                ),
+                padding: LitEdgeInsets.card,
                 child: LitGradientCard(
                   borderRadius: BorderRadius.all(
                     Radius.circular(
@@ -98,14 +112,7 @@ class _ChangeNameDialogState extends State<ChangeNameDialog> {
                     Color(0xFFF2ECE1),
                     Color(0xFFEDE7DD),
                   ],
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 8.0,
-                      color: Colors.black12,
-                      offset: Offset(-2, 4),
-                      spreadRadius: 1.0,
-                    )
-                  ],
+                  boxShadow: LitBoxShadows.md,
                   child: ConstrainedBox(
                     constraints: BoxConstraints(
                       minHeight: 64.0,
@@ -115,31 +122,10 @@ class _ChangeNameDialogState extends State<ChangeNameDialog> {
                         horizontal: 16.0,
                       ),
                       child: Center(
-                        child: Column(
-                          children: [
-                            EditableText(
-                              style: LitSansSerifStyles.body1,
-                              controller: _nameController,
-                              focusNode: _focusNode,
-                              cursorColor: LitColors.mediumGrey,
-                              backgroundCursorColor: Colors.black,
-                              cursorRadius: Radius.circular(32.0),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                top: 4.0,
-                              ),
-                              child: Container(
-                                height: 3.0,
-                                decoration: BoxDecoration(
-                                  color: LitColors.mediumGrey.withOpacity(0.4),
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(12.0),
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
+                        child: _InputField(
+                          controller: _nameController,
+                          focusNode: _focusNode,
+                          userData: widget.userData,
                         ),
                       ),
                     ),
@@ -150,6 +136,45 @@ class _ChangeNameDialogState extends State<ChangeNameDialog> {
           ),
         ),
       ),
+    );
+  }
+}
+
+/// A styled text input widget.
+///
+/// Implements a [CleanTextField] on top of a colored border.
+class _InputField extends StatelessWidget {
+  final FocusNode focusNode;
+  final TextEditingController controller;
+  final UserData userData;
+  const _InputField({
+    Key? key,
+    required this.controller,
+    required this.focusNode,
+    required this.userData,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CleanTextField(
+          style: LitSansSerifStyles.body1,
+          controller: controller,
+          focusNode: focusNode,
+          maxLines: 1,
+        ),
+        Container(
+          height: 3.0,
+          decoration: BoxDecoration(
+            color: LitColors.grey300,
+            borderRadius: BorderRadius.all(
+              Radius.circular(12.0),
+            ),
+          ),
+        ),
+        SizedBox(height: 2.0)
+      ],
     );
   }
 }
