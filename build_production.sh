d8f9933ed3a413348a1efaea7edc524757655c8c
eightgran <eightgran@gmail.com>
2022-01-24 21:14:51 +0100

    Update `build_production.sh` script - Add renaming of apk and app bundle files - Add colored text output - Delete empty folders

--- build_production.sh
+++ build_production.sh
@@ -1,14 +1,25 @@
 #/!/bin/bash
+
 now=`date +"%Y-%m-%d"`
 appName="History of Me"
-echo "Creating '${appName}' builds for ${now}."
+appNameClean="HistoryOfMe"
+arm64="arm64-v8a"
+arm32="armeabi-v7a"
+x86="x86_64"
+platform="Android"
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+reset=`tput sgr0`
+
+echo "${blue}Creating '${appName}' builds for ${now}.\n${reset}"
 echo "Building APK files ..."
 flutter build apk --split-per-abi
-echo "Finished building APK files."
-echo "Building AppBundle file."
+echo "${green}Finished building APK files.${reset}"
+echo "Building AppBundle file ...\n"
 flutter build appbundle
-echo "Finished building AppBundle file.\n"
-echo "✓ Finished creating '${appName}' builds.\n"
+echo "${green}Finished building AppBundle file.\n"
+echo "${green}✓ Finished creating '${appName}' builds.\n${reset}"
 echo "Exporting files ...\n"
 mkdir -p -v ./dist/"${now}"
 mkdir -p -v ./dist/"${now}"/apk
@@ -28,8 +39,26 @@ cat <<EOF >./dist/"${now}"/release_notes_github.txt
 EOF
 rsync -rvh --progress ./build/app/outputs/apk/ ./dist/"${now}"/apk
 rsync -rvh --progress ./build/app/outputs/bundle/ ./dist/"${now}"/appbundle
+echo "\n"
+echo "Renaming builds ...\n"
+
+# Rename and relocate arm64 build
+mv ./dist/"${now}"/apk/release/app-arm64-v8a-release.apk ./dist/"${now}"/apk/"${appNameClean}-${now}-${platform}-${arm64}.apk"
+
+# Rename and relocate arm32 build
+mv ./dist/"${now}"/apk/release/app-armeabi-v7a-release.apk ./dist/"${now}"/apk/"${appNameClean}-${now}-${platform}-${arm32}.apk"
+
+# Rename and relocate x86 build
+mv ./dist/"${now}"/apk/release/app-x86_64-release.apk ./dist/"${now}"/apk/"${appNameClean}-${now}-${platform}-${x86}.apk"
+
+# Rename and relocate app bundle
+mv ./dist/"${now}"/appbundle/release/app-release.aab ./dist/"${now}"/appbundle/"${appNameClean}-${now}-${platform}.aab"
+
 # Delete debug builds if existing
 rm -rf ./dist/"${now}"/apk/debug
-echo "\n"
-echo "✓ Finished building and exporting '${appName}' builds.\n"
-echo "Please edit the release notes before uploading your production builds.\n"
+# Delete empty folders
+rm -rf ./dist/"${now}"/apk/release
+rm -rf ./dist/"${now}"/appbundle/release
+
+echo "${green}✓ Finished building and exporting '${appName}' releases.\n${reset}"
+echo "${yellow}Please edit the release notes before uploading your production builds.\n${reset}"
